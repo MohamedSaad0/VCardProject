e5e1edcfca1142dc097bf8ce4d25915e68a14b33
mr <mohammadramadan951@gmail.com>
2022-03-17 12:42:19 +0200

    hala

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,125 +2,57 @@
 
 @section('content')
 
-     <div class="card card-primary">
+    <div class="card card-primary">
          <div class="card-header">
 
          </div>
          <!-- /.card-header -->
          <!-- form start -->
-<<<<<<< HEAD
-<form action="" method="post">
-    <div class="card-body">
-        <div class="form-group">
-            <label for="exampleInputEmail1">Full name</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter" name="name" value="{{ Auth::user()->name}}">
-        </div>
-=======
-         <div class="card-body">
-             <div id="allCompanies">
-             <div class="form-group">
-                 <label for="exampleInputEmail1">Full name</label>
+
+         <form action="/links" method="post">
+        <div class="card-body">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Full name</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter" name="name" value="{{ Auth::user()->name}}">
+            </div>
+
+            <div class="card-body">
+                <div id="allCompanies">
+
+                    <div class="form-group">
+                 <label for="exampleInputEmail1">User name</label>
                  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter name">
              </div>
 
-             <div class="form-group">
-                 <label for="exampleInputEmail1">Company name</label>
-                 <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter company name">
-             </div>
->>>>>>> 9274a5dc17fb7e9a87bbdee09e1f2dc3d3f04d77
-
-
-        <div class="form-group">
+                    <div class="form-group">
             <label for="exampleInputEmail1">Job title</label>
             <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter job" name="title" value="">
         </div>
 
-        <div class="form-group">
+                    <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
             <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" name="email" value="{{ Auth::user()->email}}">
         </div>
 
-        <div class="form-group">
-            <label for="exampleInputPassword1">Linkedin</label>
-            <input type="url" class="form-control" id="exampleInputPassword1" placeholder="Enter your Linkedin">
-        </div>
+                  <div class="form-group">
+                     <input id="newCompanyName" type="text" class="form-control my-2 " placeholder="Enter Company Name">
+                     <input id="newProfileURL" type="text" class="form-control my-2" placeholder="Enter Profile URL">
+                     <button  class="btn btn-primary" id="addNewCompany" type="button">
+                         Add
+                     </button>
 
-        <div class="form-group">
-            <label for="exampleInputPassword1">Linkedin</label>
-            <input type="url" class="form-control" id="exampleInputPassword1" placeholder="Enter your Linkedin">
-        </div>
-
-
-
-        <div class="btn-group">
-            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Small button
-            </button>
-            <ul class="dropdown-menu">
-                ...
-            </ul>
-        </div>
-
-
-
-
-
-        <div class="form-group">
-            <label for="exampleInputFile">File input</label>
-            <div class="input-group">
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="exampleInputFile">
-                    <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                </div>
-
-                <div class="input-group-append">
-                    <span class="input-group-text">Upload</span>
-                </div>
-
-
-
-<<<<<<< HEAD
-            </div>
-        </div>
-
-
-        <!-- /.card-body -->
-
-        <div class="card-footer">
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-
-    </div>
-     </div>
-</form>
-=======
-            
-             </div>
-              <div class="form-group">
-                 <input id="newCompanyName" type="text" class="form-control my-2 " placeholder="Enter Company Name">
-                 <input id="newProfileURL" type="text" class="form-control my-2" placeholder="Enter Profile URL">
-                 <button  class="btn btn-primary" id="addNewCompany" type="button">
-                     Add 
-                 </button>
-                
-             </div>
-
-                 <div class="form-group">
-                     <label for="exampleInputFile">File input</label>
-                     <div class="input-group">
-                         <div class="custom-file">
-                             <input type="file" class="custom-file-input" id="exampleInputFile">
-                             <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                         </div>
-
-                         <div class="input-group-append">
-                             <span class="input-group-text">Upload</span>
-                         </div>
-
-
-
-                     </div>
                  </div>
+
+
+             </div>
+        </div>
+    </div>
+
+</form>
+
+
+     </div>
+
 
 
              <!-- /.card-body -->
@@ -129,8 +61,7 @@
                  <button type="submit" class="btn btn-primary">Submit</button>
              </div>
 
-         </div>
-     </div>
+
      <script>
       const addNewCompany = document.querySelector("#addNewCompany");
 const newCompanyName = document.querySelector("#newCompanyName");
@@ -160,6 +91,6 @@ addNewCompany.addEventListener("click", (e) => {
 });
 
      </script>
->>>>>>> 9274a5dc17fb7e9a87bbdee09e1f2dc3d3f04d77
+
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 @endsection
